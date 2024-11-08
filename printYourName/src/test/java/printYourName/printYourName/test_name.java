d4bee7cdc70fdcf75538a96dd825566f066fc90b
nasim <nasim@gmail.com>
2024-11-08 15:03:51 -0800

    nasim

--- printYourName/src/test/java/printYourName/printYourName/test_name.java
+++ printYourName/src/test/java/printYourName/printYourName/test_name.java
@@ -4,7 +4,7 @@ public class test_name {
 
 	public static void main(String[] args) {
 		System.out.println("Hi, My name is Maksuda");
-		System.out.println("Hi! my name is nasim");
+		System.out.println("Hi! My name is nasim");
 	}
 
 }
